2b123f355fe6add2f4ca2e63885ac2378f747abc
RodoGomez <rodgom@protonmail.com>
2021-02-22 16:11:02 -0600

    docs updated with new post

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Radare2: Basic workflow and usage - Part 1",
+        "excerpt":"This is the first post out of two that describes the usage of Radare2 to perform basic tasks that relate to binary analysis. That way, instead of explaining what a bunch of Radare commands do, this post goes through a basic workflow from opening a binary file, gather basic information...","categories": [],
+        "tags": [],
+        "url": "/radare2-basic-workflow-and-usage",
+        "teaser": null
+      }]

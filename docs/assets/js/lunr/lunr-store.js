a81dd0708d1e120bc1ef828890fe85d1d92d2dee
RodoGomez <rodgom@protonmail.com>
2021-04-12 14:35:54 -0500

    New post added.! Computer Architecture 101

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -10,4 +10,10 @@ var store = [{
         "tags": [],
         "url": "/radare2-basic-workflow-and-usage-part2",
         "teaser": null
+      },{
+        "title": "x86 Architecture: Architecture 101",
+        "excerpt":"Welcome to this new series of posts, this time we’ll be talking about x86-64 computer architecture, specifically, on the firsts posts, I’ll give a very very basic introduction to computer architecture, how it works, CPU, memory and more, then in future posts we’ll discuss the hardware support that Intel 64...","categories": [],
+        "tags": [],
+        "url": "/x86arch-part1-intro-101",
+        "teaser": null
       }]
